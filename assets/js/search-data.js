25e200b953d69e0f97acfd0576105c5c0c9f1129
pam-alamilla <85764176+pam-alamilla@users.noreply.github.com>
2025-05-12 01:56:14 +0000

    Deploying to gh-pages from @ pam-alamilla/pam-alamilla.github.io@88ce376c56eda2a274e73e56094d5aaf9499d829 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,21 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/news/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "books-the-godfather",
+        },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
